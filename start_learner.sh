cb6da34ff132bc82a8605879eb9ddb8050e3fe6e
hendrix-dai <865058222@qq.com>
2021-05-06 20:12:20 +0800

    before change url to real ip

--- start_learner.sh
+++ start_learner.sh
@@ -1,6 +1,6 @@
-python learner.py --batch_size 8 --num_buffers 24 --env TimePilotNoFrameskip-v4 --unroll_length 64 --mode train  --num_actors 3  \
+python learner.py --batch_size 8 --num_buffers 24 --env MsPacmanNoFrameskip-v4 --unroll_length 64 --mode train  --num_actors 6  \
     --learning_rate 0.0004 \
     --epsilon 0.01 \
     --entropy_cost 0.01 \
     --total_steps 6100000 \
-    --remark "cut_at_layer_0_actor_3_TimePilot"
+    --remark "cut_at_layer_0_actor_6_MsPacman_testNetworkFlow"
